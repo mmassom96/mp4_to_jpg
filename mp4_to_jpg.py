70d85428daa015cbaa6b25821ee6141762b3a50d
Matthew Massom <m.massom96@gmail.com>
2019-10-02 11:27:52 -0500

    Added user-defined directory input and bug fixes

--- mp4_to_jpg.py
+++ mp4_to_jpg.py
@@ -1,9 +1,38 @@
+# Matthew Massom
+# github.com/mmassom96
+# mp4_to_jpg.py
+# October 2, 2019
+#
+# This program was written to convert a .MP4 video file into .jpg image files of
+# each individual frame of the video. The purpose of this program is to prepare
+# images from a video that can then be annotated for training in a YOLOv3 object
+# detection network. By design, it will convert every .MP4 file that it finds
+# within the user-defined directory. Each video file's frame images will be saved
+# in a folder of the same name as the respective video under the user-defined
+# output directory.
+
+
 import cv2
-import numpy
 import os
 
-inputDir = '/home/matthew/Videos/GoPro_Test/'
-outputDir = '/media/matthew/KINGSTON/GoPro_Test/img_files/'
+while(True):
+    inputDir = input('Define the full input directory: ')
+    if not inputDir.endswith('/'):
+        inputDir = inputDir + '/'
+    while(True):
+        correct = input('You entered ' + inputDir + ' is that correct? [Y/n]: ')
+        if correct == 'y' or correct == 'Y' or correct == 'n' or correct == 'N': break
+    if correct == 'y' or correct == 'Y': break
+
+while(True):
+    outputDir = input('Define the full output directory: ')
+    if not outputDir.endswith('/'):
+        outputDir = outputDir + '/'
+    while(True):
+        correct = input('You entered ' + outputDir + ' is that correct? [Y/n]: ')
+        if correct == 'y' or correct == 'Y' or correct == 'n' or correct == 'N': break
+    if correct == 'y' or correct == 'Y': break
+
 
 for filename in os.listdir(inputDir):
     if filename.endswith(".MP4"):
@@ -13,7 +42,7 @@ for filename in os.listdir(inputDir):
             if not os.path.exists(outputDir + newDir):
                 os.makedirs(outputDir + newDir)
         except OSError:
-            print ('Error: Creating directory of', outputDir, newDir)
+            print ('Error: Creating directory: ', outputDir, newDir)
 
         currentFrame = 0
 
@@ -21,7 +50,7 @@ for filename in os.listdir(inputDir):
         ret, frame = cap.read()
         if not ret: break
         name = outputDir + newDir + str(currentFrame) + '.jpg'
-        print('Creating frame', currentFrame, 'of', filename)
+        print('Extracting frame', currentFrame, 'of', filename)
         cv2.imwrite(name, frame)
         currentFrame += 1
 
